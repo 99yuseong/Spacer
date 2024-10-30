ee10caa8a82ead3ce5350b0d05011383d7411167
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-10-30 22:17:49 +0900

    Create Swift Package

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Spacer",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Spacer",
+            targets: ["Spacer"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Spacer"),
+        .testTarget(
+            name: "SpacerTests",
+            dependencies: ["Spacer"]
+        ),
+    ]
+)
